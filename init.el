37283bf8f2c35eba24a7ea6c4f3928e0057eed53
Misha Pachin <misha.pachin@gmail.com>
2017-12-18 16:07:01 +0300

    initial commit

---
+++ init.el
@@ -0,0 +1,77 @@
+;; #############################################
+;; ##### initializing package repositories #####
+;; #############################################
+
+(require 'package)
+(package-initialize)
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("marmalade" . "https://marmalade-repo.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
+
+;; #############################################
+;; ################### ??? #####################
+;; #############################################
+;; custom-set-variables was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+(custom-set-variables
+ '(package-selected-packages (quote (avy company-tern xref-js2 js2-refactor js2-mode))))
+(custom-set-faces
+ )
+
+;; #############################################
+;; ################ js2-mode ###################
+;; #############################################
+
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;; Better imenu
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+
+;; #############################################
+;; ######### js2-refactor + xref-js2 ###########
+;; #############################################
+
+(require 'js2-refactor)
+(require 'xref-js2)
+
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+
+;; js-mode (which js2 is based on) binds "M-." which conflicts with
+;; xref, so unbind it
+
+(define-key js-mode-map (kbd "M-.") nil)
+
+(add-hook 'js2-mode-hook (lambda ()
+   (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
+;; #############################################
+;; ################# tern  #####################
+;; #############################################
+
+(require 'company)
+(require 'company-tern)
+
+(add-to-list 'company-backends 'company-tern)
+(add-hook 'js2-mode-hook (lambda ()
+			   (tern-mode)
+			   (company-mode)))
+;; Disable completeon keybindings, as wo use xref-js2 instead
+;; (define-key tern-mode-keymap (kbd "M-.") nil)
+;; (define-key tern-mode-keymap (kbd "M-,") nil)
+
+;; #############################################
+;; ################## avy  #####################
+;; #############################################
+
+;; (global-set-key (kbd "C-c j") 'avy-goto-word-or-subword-1)
+;; (global-set-key (kbd "s-.") 'avy-goto-word-or-subword-1)
+;; (global-set-key (kbd "s-w") 'ace-window)
+
+(global-set-key (kbd "C-.") 'avy-goto-char)
+(global-set-key (kbd "C-,") 'avy-goto-line)
